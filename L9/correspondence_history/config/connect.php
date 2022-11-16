22a8a9d2029b5137c3d7b01da45535f02b83f586
Mykola010 <114877776+Mykola010@users.noreply.github.com>
2022-11-16 21:59:54 +0200

    Delete L9/correspondence_history directory

--- L9/correspondence_history/config/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-$connect = mysqli_connect('localhost', 'root', '', 'correspondence_history');
-if(!$connect) {
-  die('Ошибка подключения к БД');
-}
--- L9/correspondence_history/vendor/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../config/connect.php';
-
-$id = $_GET['id'];
-
-mysqli_query($connect, "DELETE FROM correspondence_history WHERE `correspondence_history`.`id` ='$id'");
-
-header('Location: ../index.php');
